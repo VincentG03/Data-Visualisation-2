f6c8c29618f471f413c81bdb09f1c3be05f0547e
VincentG03 <vincentgiang.aus@gmail.com>
2024-10-04 17:26:54 +1000

    (!!!) Base HTML working

--- charts.js
+++ charts.js
@@ -1,33 +1,33 @@
-var vg_1 = "T1_groupedbar.vg.json"; // FILE_NAME.vg.json
-vegaEmbed("#map_graph", vg_1).then(function(result) { //INDEX_HTML reference
+var vg_1 = "chart/T1_groupedbar.vg.json"; // FILE_NAME.vg.json
+vegaEmbed("#T1_groupedbar", vg_1).then(function(result) { //INDEX_HTML reference
 }).catch(console.error);
 
 
-var vg_3 = "T3_choropleth.vg.json"; 
-vegaEmbed("#map_graph", vg_1).then(function(result) { 
+var vg_3 = "chart/T3_choropleth.vg.json"; 
+vegaEmbed("#T3_choropleth", vg_3).then(function(result) { 
 }).catch(console.error);
 
 
-var vg_4_1 = "T4_horzpercentagebar.vg.json"; 
-vegaEmbed("#map_graph", vg_1).then(function(result) { 
+var vg_4_1 = "chart/T4_horzpercentagebar.vg.json"; 
+vegaEmbed("#T4_horzpercentagebar", vg_4_1).then(function(result) { 
 }).catch(console.error);
 
 
-var vg_4_2 = "T4_horzstackedbar.vg.json"; 
-vegaEmbed("#map_graph", vg_1).then(function(result) { 
+var vg_4_2 = "chart/T4_horzstackedbar.vg.json"; 
+vegaEmbed("#T4_horzstackedbar", vg_4_2).then(function(result) { 
 }).catch(console.error);
 
 
-var vg_6 = "T6_pie.vg.json"; 
-vegaEmbed("#map_graph", vg_1).then(function(result) { 
+var vg_6 = "chart/T6_pie.vg.json"; 
+vegaEmbed("#T6_pie", vg_6).then(function(result) { 
 }).catch(console.error);
 
 
-var vg_8 = "T8_stackedbar.vg.json"; 
-vegaEmbed("#map_graph", vg_1).then(function(result) { 
+var vg_8 = "chart/T8_stackedbar.vg.json"; 
+vegaEmbed("#T8_stackedbar", vg_8).then(function(result) { 
 }).catch(console.error);
 
 
-var vg_11 = "T11_groupedbar.vg.json"; 
-vegaEmbed("#map_graph", vg_1).then(function(result) { 
+var vg_11 = "chart/T11_groupedbar.vg.json"; 
+vegaEmbed("#T11_groupedbar", vg_11).then(function(result) { 
 }).catch(console.error);
