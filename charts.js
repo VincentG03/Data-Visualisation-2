1b4d733f34c1753f55cd9fe23142089b098ffcf7
VincentG03 <vincentgiang.aus@gmail.com>
2024-10-04 17:02:31 +1000

    Added and filled out "chart.js"

---
+++ charts.js
@@ -0,0 +1,33 @@
+var vg_1 = "T1_groupedbar.vg.json"; // FILE_NAME.vg.json
+vegaEmbed("#map_graph", vg_1).then(function(result) { //INDEX_HTML reference
+}).catch(console.error);
+
+
+var vg_3 = "T3_choropleth.vg.json"; 
+vegaEmbed("#map_graph", vg_1).then(function(result) { 
+}).catch(console.error);
+
+
+var vg_4_1 = "T4_horzpercentagebar.vg.json"; 
+vegaEmbed("#map_graph", vg_1).then(function(result) { 
+}).catch(console.error);
+
+
+var vg_4_2 = "T4_horzstackedbar.vg.json"; 
+vegaEmbed("#map_graph", vg_1).then(function(result) { 
+}).catch(console.error);
+
+
+var vg_6 = "T6_pie.vg.json"; 
+vegaEmbed("#map_graph", vg_1).then(function(result) { 
+}).catch(console.error);
+
+
+var vg_8 = "T8_stackedbar.vg.json"; 
+vegaEmbed("#map_graph", vg_1).then(function(result) { 
+}).catch(console.error);
+
+
+var vg_11 = "T11_groupedbar.vg.json"; 
+vegaEmbed("#map_graph", vg_1).then(function(result) { 
+}).catch(console.error);
